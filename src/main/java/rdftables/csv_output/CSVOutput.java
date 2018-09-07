a3e2a5be81be9fdc6367fa9f19c257f3c05b887c
Greg Albiston <greg_albiston@hotmail.com>
2018-09-07 13:48:56 +0100

    - changed "csv_output" package to "rdftables.csv_output" for consistency.

--- src/main/java/rdftables/csv_output/CSVOutput.java
+++ src/main/java/rdftables/csv_output/CSVOutput.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package csv_output;
+package rdftables.csv_output;
 
 import java.util.HashMap;
 import java.util.List;
